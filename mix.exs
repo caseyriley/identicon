96052af0c0f7e88ef1a477d92092e88b26f446ec
caseyriley <innerforest7@gmail.com>
2021-09-16 18:16:27 -0700

    make identicon.png

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Identicon.MixProject do
     [
       app: :identicon,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +21,7 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:egd, github: "erlang/egd"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
